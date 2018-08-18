f439d27fb04482cdd0fb6438ce4b862550c72f1f
Yu Suzuki <ysuzuki@is.naist.jp>
2018-08-18 21:35:08 +0900

    set media parameter

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.logger = Logger.new('log/trace.log', 14, 10 * 1024 * 1024)
+  #config.logger = Logger.new('log/trace.log', 14, 10 * 1024 * 1024)
 end
