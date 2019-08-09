8b02a8477d0c93a955bd0135aec5bc2e5bf534ae
Yu Suzuki <suzuki_yu@icloud,com>
2019-08-09 16:09:37 +0900

    Use postgresql insted of Oracle

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,26 @@
+class SearchController < ApplicationController
+  def search
+    keywords = params[:keyword]
+    lang = params[:lang]
+    deleted = params[:deleted]
+    mention = params[:mention]
+    retweet = params[:retweet]
+    data = nil
+    query = ""
+    keywords.split(',').each do |k|
+      query << k
+      query << " OR "
+    end
+    query.slice!(-4,4)
+    data = TweetText.where("text &@~ ?", query)
+    data = data.where(lang: lang) if lang && data
+    data = data.where(deleted: deleted) if deleted && data
+    data = data.where(mention: mention) if mention && data
+    data = data.where(retweet: retweet) if retweet && data
+    render json: {status: 'SUCCESS', count: data.size, query: query,  data: data}
+  end
+
+  def search_params
+    params.require(:keyword).permit(:lang, :deleted, :mention, :retweet)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'search/search'
   resources :parameters
   get 'home/index'
   root to: 'home#index'
