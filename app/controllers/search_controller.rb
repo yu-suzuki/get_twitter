1dee2deb75b8bff86168f467cf36cfa130f2ffea
Yu Suzuki <suzuki_yu@icloud,com>
2019-08-19 12:16:32 +0900

    Use postgresql insted of Oracle

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -10,27 +10,32 @@ class SearchController < ApplicationController
     id_min = search_params[:id_min]
     data = nil
     query = ""
-    keywords.split(',').each do |k|
-      query << k
-      query << " OR "
-    end
 
-    query.slice!(-4, 4)
-    data = TweetText.all
-    data = data.where("text &@~ ?", query)
-    data = data.where(lang: lang) if lang && data
-    data = data.where(deleted: deleted) if deleted && data
-    data = data.where(mention: mention) if mention && data
-    data = data.where(retweet: retweet) if retweet && data
-    data = data.where(reply: reply) if reply && data
-    data = data.where("id > ?", id_min) if id_min && data
-    data = data.where("id < ?", id_max) if id_max && data
-    data = data.order(id: :desc).limit(100)
-    if data.empty?
-      render json: {status: 'ERROR', message: 'Not found'}
-    else
-      render json: {status: 'SUCCESS', count: data.size, query: query,
-                    id_max: data.first.id, id_min: data.last.id, data: data}
+    begin
+      keywords.split(',').each do |k|
+        query << k
+        query << " OR "
+      end
+
+      query.slice!(-4, 4)
+      data = TweetText.all
+      data = data.where("text &@~ ?", query)
+      data = data.where(lang: lang) if lang && data
+      data = data.where(deleted: deleted) if deleted && data
+      data = data.where(mention: mention) if mention && data
+      data = data.where(retweet: retweet) if retweet && data
+      data = data.where(reply: reply) if reply && data
+      data = data.where("id > ?", id_min) if id_min && data
+      data = data.where("id < ?", id_max) if id_max && data
+      data = data.order(id: :desc).limit(100)
+      if data.empty?
+        render json: {status: 'ERROR', message: 'Not found'}
+      else
+        render json: {status: 'SUCCESS', count: data.size, query: query,
+                      id_max: data.first.id, id_min: data.last.id, data: data.select(:id,:tweet_user_id, :text, :created_at)}
+      end
+    rescue NoMethodError
+      render json: {status: 'ERROR', message: 'Invalid Query'}
     end
 
   end
