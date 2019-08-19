986b3d9750197a6713f3f4329a5ddbcd48e6aa46
Yu Suzuki <suzuki_yu@icloud,com>
2019-08-19 15:40:34 +0900

    Use postgresql insted of Oracle

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -13,10 +13,10 @@ class SearchController < ApplicationController
     begin
       keywords.split(',').each do |k|
         query << k
-        query << " AND "
+        query << " "
       end
 
-      query.slice!(-4, 4)
+      query.slice!(-1, 1)
       data = TweetText.all
       data = data.where("text &@~ ?", query)
       data = data.where(lang: lang) if lang && data
@@ -27,13 +27,13 @@ class SearchController < ApplicationController
       data = data.where("id < ?", id_max) if id_max && data
       data = data.order(id: :desc).limit(100)
       if data.empty?
-        render json: {status: 'ERROR', message: 'Not found'}
+        render json: {status: 'ERROR', message: 'Not found', query: query}
       else
         render json: {status: 'SUCCESS', count: data.size, query: query,
                       id_max: data.first.id, id_min: data.last.id, data: data.select(:id,:tweet_user_id, :text, :created_at)}
       end
-    rescue NoMethodError
-      render json: {status: 'ERROR', message: 'Invalid Query'}
+    rescue NoMethodError => e
+      render json: {status: 'ERROR', message: e}
     end
 
   end
